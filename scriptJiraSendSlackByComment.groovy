7e753335555bc43248318c872f390b5c6748a7f3
Gabriel Tessarini <gtessarini@uol.com.br>
2018-11-29 16:30:37 -0200

    scriptJiraSendSlackByComment - Implementing code
    A script to add on Jira Script Runner "Script Listener" with method "Comment Created" to send automatically a message on Slack with comment author and content to user with identifier on task label when a comment is made on task

---
+++ scriptJiraSendSlackByComment.groovy
@@ -0,0 +1,31 @@
+//Define the labels with name of each user and set it slack conversation links
+def slacks = [
+    ["name": "ELVIS", "link": "https://hooks.slack.com/services/XXXXXXX/YYYYYYY/ZZZZZZZZZZZZZZZZZZZ"],
+    ["name": "PRISICLA", "link": "https://hooks.slack.com/services/XXXXXXX/YYYYYYY/ZZZZZZZZZZZZZZZZZZZ"]
+]
+
+//Get the issue of comment
+def thisIssue = get("/rest/api/2/issue/" + issue["id"] + "")
+    .asObject(Map)
+    .body
+   
+//Get all label values in this issue
+def labels = thisIssue.fields["labels"];
+
+//Send a Slack for each user with label in this issue
+for(label in labels){
+    for(slack in slacks){
+        if(slack.name == label){
+            post(slack.link)
+                .header("Content-Type", "application/json")
+                .body(
+                    [
+                        text: "JIRA - You receive a comment from user '" + comment.author.displayName + "':\n" + comment.body
+                    ]
+                )
+                .asString()
+        }
+    }
+}
+
+/* Gabriel Tessarini */
